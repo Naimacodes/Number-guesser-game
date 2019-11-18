7a66913ea9d3dc583b8ae05133e9f584466d0f0b
Naima <naima.adanahmed@gmail.com>
2019-11-18 13:30:13 +0100

    added function to write an error message and an eventListener on the submit button

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ const game = document.querySelector('#game');
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
       guessBtn = document.querySelector('#guess-btn'),
-      guessinput = document.querySelector('.message');
+      guessInput = document.querySelector('#guess-input');
+      message = document.querySelector(".message");
 
 minNum.textContent = min;
 maxNum.textContent = max;
 
+guessBtn.addEventListener('click', function(){
+  let guess = parseInt(guessInput.value);
+
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between  ${min} and ${max}.`, 'red');
+  }
+
+ 
+});
+
+function setMessage(msg, color){
+  message.style.color = color;
+  message.textContent = msg;
+};
